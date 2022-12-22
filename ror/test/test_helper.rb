4cf472bd6348f8ffda9c9405c1fd874a9b853ebc
Ali Quintanilha <alipq7@gmail.com>
2022-12-22 19:24:41 -0300

    Create integration tests

--- ror/test/test_helper.rb
+++ ror/test/test_helper.rb
@@ -11,5 +11,8 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  # Make full title helper available to test suite
+  class ActiveSupport::TestCase
+    include FullTitleHelper 
+  end
 end
