9f4ca470ff5ed2a854458e24204f6ad74c47ebe8
Ali Quintanilha <alipq7@gmail.com>
2022-12-23 17:17:35 -0300

    Update user model including secure password

---
+++ ror/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # User name validations
+  validates :name, presence: true, length: { maximum: 255 }
+  
+  # User email validations
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+
+  # User password validations
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
