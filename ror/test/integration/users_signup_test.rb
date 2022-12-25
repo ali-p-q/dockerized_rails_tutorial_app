a85a1f07a4724eb344abb703f714befb55160692
Ali Quintanilha <alipq7@gmail.com>
2022-12-25 19:16:52 -0300

    Add signup feature

---
+++ ror/test/integration/users_signup_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    @invalid_user = User.new
+  end
+
+  test 'should not submit invalid user on signup form' do
+    assert_no_difference 'User.count' do
+      get signup_path
+      post users_path, params: { user: {  name: '',
+                                          email: 'user@example',
+                                          password: 'foobar',
+                                          password_confirmation: 'foobar' } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+  end
+
+  test 'should submit valid user on signup form' do
+    assert_difference 'User.count', 1 do
+      get signup_path
+      post users_path, params: { user: {  name: 'user',
+                                          email: 'user@example.com',
+                                          password: 'foobar',
+                                          password_confirmation: 'foobar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
