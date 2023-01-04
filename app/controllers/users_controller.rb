bdbefde6d814e28049f868442f53761b3484fa87
Ali Quintanilha <alipq7@gmail.com>
2023-01-04 18:04:23 -0300

    fix: Change redirect_to from _path to _url
    This change is made to comply with HTTP specification
    (Location: header in 3xx redirects is a complete URL)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
   def show
     @user = User.find(params[:id])
   end
@@ -41,5 +42,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    flash[:danger] = t('flash.users.assert.login.failure')
+    redirect_to(login_url)
   end
 end
